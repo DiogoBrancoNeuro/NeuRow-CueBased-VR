b0ad1bc929c8fdcf82be13c28a59f1407afac52b
DiogoBrancoNeuro <55894612+DiogoBrancoNeuro@users.noreply.github.com>
2020-12-08 14:39:40 +0000

    Training Phase modified
    Grips both hands.
    Code changes in RotateBrush.

--- Assets/VR-RHI/RotateBrush.cs
+++ Assets/VR-RHI/RotateBrush.cs
@@ -7,20 +7,29 @@ public class RotateBrush : MonoBehaviour
     public Vector3 BrushPosition;
     public float velocity;
     public GameObject Brush;
-
+    public float a;
+    public Vector3 OriginalRotation;
+    public Vector3 Rotating;
     // Start is called before the first frame update
     void Start()
     {
         velocity = 60;
-        
-        
+        OriginalRotation = GetComponent<Transform>().localRotation.eulerAngles;
+        Rotating = Vector3.up * Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up*velocity*Time.fixedDeltaTime);
 
+        transform.Rotate(Rotating * velocity);
+        Debug.Log("Rotating: " + Rotating);
+        Debug.Log("OriginalRotation: " + OriginalRotation);
+        if (Rotating == OriginalRotation)
+        {
+            a++;
+        }
+        
         // Make it vibrate sometimes
         //	OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
 
