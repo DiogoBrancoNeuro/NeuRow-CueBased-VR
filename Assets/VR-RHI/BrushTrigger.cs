b14e5008b6af8c195ec1b325b79734760c92f2bd
DiogoBrancoNeuro <55894612+DiogoBrancoNeuro@users.noreply.github.com>
2020-12-09 01:17:29 +0000

    Boat_training now spinning with a trigger!
    Grips and Brush now have a trigger near the hand.
    Note: There is a bug in which if the left hand has a rigidbody component it will break (jump out of screen). This bug can be ignore since both hands and triggers work at the same time.

---
+++ Assets/VR-RHI/BrushTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrushTrigger : MonoBehaviour
+{
+    public GameObject collisionobject;
+    public int a;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CollisionObjectTrigger")
+            Debug.Log("TriggerCollision");
+    }
+    
+
+}
--- Assets/VR-RHI/RotateBrush.cs
+++ Assets/VR-RHI/RotateBrush.cs
@@ -5,33 +5,39 @@ using UnityEngine;
 public class RotateBrush : MonoBehaviour
 {
     public Vector3 BrushPosition;
-    public float velocity;
+    public float velocity=60;
     public GameObject Brush;
     public float a;
     public Vector3 OriginalRotation;
     public Vector3 Rotating;
+    public Vector3 LastRotationPosition;
     // Start is called before the first frame update
     void Start()
     {
-        velocity = 60;
-        OriginalRotation = GetComponent<Transform>().localRotation.eulerAngles;
-        Rotating = Vector3.up * Time.deltaTime;
+        
+        OriginalRotation = GetComponent<Transform>().rotation.eulerAngles;
+        
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        transform.Rotate(Rotating * velocity);
-        Debug.Log("Rotating: " + Rotating);
-        Debug.Log("OriginalRotation: " + OriginalRotation);
-        if (Rotating == OriginalRotation)
+        Rotating = Vector3.up * Time.deltaTime * velocity;
+        
+        transform.Rotate(Rotating);
+        
+        if (OriginalRotation == LastRotationPosition)
         {
-            a++;
+            Debug.Log("Trigger");
         }
         
+
+        // Send a trigger to the eeg, everytime the brush touches the hand.
         // Make it vibrate sometimes
         //	OVRInput.SetControllerVibration(1, 1, OVRInput.Controller.RTouch);
 
     }
+    
 }
